4c04e99a9784d14e47b7bd9dc6606f42225d1dd7
dramos <dario.fenix@gmail.com>
2017-07-28 16:36:24 -0300

    DEV-8: Step 1: Use bitprim's CMakeLists.txt instead of the sample one, set target library name and metadata

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,9 @@ from conans import ConanFile, CMake
 class BitprimcoreConan(ConanFile):
     name = "bitprim-core"
     version = "0.1"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "Description of Bitprimcore"
+    license = "http://www.boost.org/users/license.html"
+    url = "https://github.com/bitprim/bitprim-core"
+    description = "Bitcoin Cross-Platform C++ Development Toolkit"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
@@ -15,8 +15,8 @@ class BitprimcoreConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        self.run('cmake %s/src %s' % (self.source_folder, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
         self.copy("*.h", dst="include", src="src")
@@ -27,4 +27,4 @@ class BitprimcoreConan(ConanFile):
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["bitprim-core"]
