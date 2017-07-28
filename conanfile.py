931f44234ee1ef9c5f2f7787dd328edfb4934d88
dramos <dario.fenix@gmail.com>
2017-07-28 17:04:21 -0300

    DEV-8: Step 2: Export CMakeLists.txt, because it is not inside src/

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class BitprimcoreConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-    exports_sources = "src/*"
+    exports_sources = "src/*", "CMakeLists.txt"
 
     def build(self):
         cmake = CMake(self)
