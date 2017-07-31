83d8090dfb60c2de2bc8d64631c2375ead36b9a7
dramos <dario.fenix@gmail.com>
2017-07-31 18:43:38 -0300

    DEV-8: Do only release builds

--- build.py
+++ build.py
@@ -5,4 +5,9 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable")
     builder.add_common_builds(shared_option_name="bitprim-core:shared")
     builder.password = os.getenv("CONAN_PASSWORD")
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["build_type"] == "Release":
+             filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
