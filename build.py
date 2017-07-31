c7a4012e6e6db30f15cd31ca6e8df3ceebde28bc
dramos <dario.fenix@gmail.com>
2017-07-31 01:43:18 -0300

    DEV-8: Bintray credentials

--- build.py
+++ build.py
@@ -4,4 +4,5 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable")
     builder.add_common_builds(shared_option_name="bitprim-core:shared")
+    builder.password = os.getenv("CONAN_PASSWORD")
     builder.run()
