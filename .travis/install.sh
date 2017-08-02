2d0fa52c3dc2f57127cfa71d72797717dd6136dc
Dario Ramos <dario.fenix@gmail.com>
2017-08-02 20:27:52 -0300

    DEV-8: Add remotes for Bitprim's bzip2 and zlib

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,7 +22,9 @@ fi
 pip install conan --upgrade
 pip install conan_package_tools
 
-conan remote add conan_boost https://api.bintray.com/conan/bitprim/bitprim-conan-boost
+conan remote add bitprim_conan_boost https://api.bintray.com/conan/bitprim/bitprim-conan-boost
 conan remote add bitprim_sec https://api.bintray.com/conan/bitprim/secp256k1
+conan remote add bitprim_conan_bzip2 https://api.bintray.com/conan/bitprim/bitprim-conan-bzip2
+conan remote add bitprim_conan_zlib https://api.bintray.com/conan/bitprim/bitprim-conan-zlib
 
 conan user
