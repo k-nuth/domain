6b1d8357de929d8cd031aac63e0dbef51c3b72eb
Dario Ramos <dario.fenix@gmail.com>
2017-08-14 18:26:29 -0300

    DEV-10: Raise verbosity

--- conanfile.py
+++ conanfile.py
@@ -20,6 +20,7 @@ class BitprimcoreConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = "ON"
         cmake.configure(source_dir=self.conanfile_directory)
         cmake.build()
 
