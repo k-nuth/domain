cc675f533be071d4519db3b0f861fe29d7b7347e
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-18 09:08:41 -0300

    fix for mingw conan package

--- build.py
+++ build.py
@@ -1,14 +1,4 @@
-# import os
 from conan.packager import ConanMultiPackager
-
-#def is_clang_or_gcc5(sett):
-#    # precondition: sett["compiler"] and sett["compiler.version"] are valid
-#    return sett["compiler"] == "clang" or (sett["compiler"] == "gcc" and float(sett["compiler.version"]) >= 5)
-
-#def replace_libcxx(sett):
-#    if "compiler" in sett and "compiler.libcxx" in sett and "compiler.version" in sett and is_clang_or_gcc5(sett) and sett["compiler.libcxx"] == "libstdc++":
-#        sett["compiler.libcxx"] = "libstdc++11"
-#    return sett
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="testing",
@@ -18,33 +8,25 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="bitprim-core:shared", pure_c=False)
 
     filtered_builds = []
-    # add_special_case = False
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
                 and options["bitprim-core:shared"] == False:
 
             #settings = replace_libcxx(settings)
-            #filtered_builds.append([settings, options, env_vars, build_requires])
+            filtered_builds.append([settings, options, env_vars, build_requires])
 
             # #TODO: Adding gcc 4.9 c++11 build manually until Conan fixes it
             # if settings["compiler"] == "gcc" and settings["compiler.version"] == "4.9":
-            #     add_special_case = True
+            #     # new_settings={'compiler.version': '4.9', 'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'}, options={'bitprim-conan-boost:shared': False})
+            #     # new_settings = {'compiler.libcxx': settings["compiler.libcxx"], 'compiler.exception': settings["compiler.exception"], 'compiler.threads': settings["compiler.threads"], 'compiler.version': settings["compiler.version"], 'arch': settings["arch"], 'build_type': settings["build_type"], 'compiler': settings["compiler"]}
+            #     new_settings = {'compiler.libcxx': 'libstdc++11', 'compiler.exception': settings["compiler.exception"], 'compiler.threads': settings["compiler.threads"], 'compiler.version': settings["compiler.version"], 'arch': settings["arch"], 'build_type': settings["build_type"], 'compiler': settings["compiler"]}
+            #     filtered_builds.append([new_settings, options, env_vars, build_requires])
+
+            #     # ERROR: C:\Users\appveyor\.conan\data\mingw_installer\1.0\conan\stable\export\conanfile.py: Error while initializing settings. Invalid setting 'libstdc++11' is not a valid 'settings.compiler.libcxx' value.
+            #     # Possible values are ['libstdc++']
+
             # else:
-            #     if not "compiler.libcxx" in settings or settings["compiler"] == "apple-clang" or ("compiler.libcxx" in settings and settings["compiler.libcxx"] == "libstdc++"):
-            #         filtered_builds.append([settings, options, env_vars, build_requires])
-
-
-            #TODO: Adding gcc 4.9 c++11 build manually until Conan fixes it
-            if settings["compiler"] == "gcc" and settings["compiler.version"] == "4.9":
-                # new_settings={'compiler.version': '4.9', 'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'}, options={'bitprim-conan-boost:shared': False})
-                # new_settings = {'compiler.libcxx': settings["compiler.libcxx"], 'compiler.exception': settings["compiler.exception"], 'compiler.threads': settings["compiler.threads"], 'compiler.version': settings["compiler.version"], 'arch': settings["arch"], 'build_type': settings["build_type"], 'compiler': settings["compiler"]}
-                new_settings = {'compiler.libcxx': 'libstdc++11', 'compiler.exception': settings["compiler.exception"], 'compiler.threads': settings["compiler.threads"], 'compiler.version': settings["compiler.version"], 'arch': settings["arch"], 'build_type': settings["build_type"], 'compiler': settings["compiler"]}
-                filtered_builds.append([new_settings, options, env_vars, build_requires])
-            else:
-                filtered_builds.append([settings, options, env_vars, build_requires])
-
+            #     filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
-    # if add_special_case:
-    #     builder.add(settings={'compiler.version': '4.9', 'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'}, options={'bitprim-conan-boost:shared': False})
     builder.run()
