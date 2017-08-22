cb40375de874049f439c603f023e0325213f2b64
Dario Ramos <dario.fenix@gmail.com>
2017-08-22 19:49:10 -0300

    DEV-33: Control C++11 ABI flag from Conan

--- conanfile.py
+++ conanfile.py
@@ -43,6 +43,7 @@ class BitprimcoreConan(ConanFile):
                "with_png": [True, False],
                "with_litecoin": [True, False],
                "with_qrencode": [True, False],
+               "use_cpp11_abi": [True, False]
     }
 
     default_options = "shared=False", \
@@ -52,7 +53,8 @@ class BitprimcoreConan(ConanFile):
         "with_icu=False", \
         "with_png=False", \
         "with_litecoin=False", \
-        "with_qrencode=False"
+        "with_qrencode=False", \
+        "use_cpp11_abi=False"
 
 
 # option(USE_CONAN "Use Conan Build Tool." OFF)
@@ -80,6 +82,7 @@ class BitprimcoreConan(ConanFile):
         cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = "ON"
         cmake.definitions["ENABLE_SHARED"] = option_on_off(self.options.shared)
         cmake.definitions["ENABLE_POSITION_INDEPENDENT_CODE"] = option_on_off(self.options.fPIC)
+        cmake.definitions["USE_CPP11_ABI"] = option_on_off(self.options.use_cpp11_abi)
         cmake.definitions["WITH_TESTS"] = option_on_off(self.options.with_tests)
         cmake.definitions["WITH_EXAMPLES"] = option_on_off(self.options.with_examples)
         cmake.definitions["WITH_ICU"] = option_on_off(self.options.with_icu)
