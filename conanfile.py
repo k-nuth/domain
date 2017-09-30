18f64a46d031ca15ded67157be171e269d2fc99f
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-09-30 11:41:14 -0300

    [skip ci] 0.3 version and avoid tests by default

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ def option_on_off(option):
 
 class BitprimcoreConan(ConanFile):
     name = "bitprim-core"
-    version = "0.2"
+    version = "0.3"
     license = "http://www.boost.org/users/license.html"
     url = "https://github.com/bitprim/bitprim-core"
     description = "Bitcoin Cross-Platform C++ Development Toolkit"
@@ -48,7 +48,7 @@ class BitprimcoreConan(ConanFile):
 
     default_options = "shared=False", \
         "fPIC=True", \
-        "with_tests=True", \
+        "with_tests=False", \
         "with_examples=False", \
         "with_icu=False", \
         "with_png=False", \
@@ -73,7 +73,7 @@ class BitprimcoreConan(ConanFile):
     build_policy = "missing"
 
     requires = (("bitprim-conan-boost/1.64.0@bitprim/stable"),
-               ("secp256k1/0.2@bitprim/testing"))
+               ("secp256k1/0.3@bitprim/testing"))
 
     def build(self):
         cmake = CMake(self)
