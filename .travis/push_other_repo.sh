01922cd0d65e3a77d88b6e88cd6caa2465233db3
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-11-02 11:13:32 -0300

    fixes

---
+++ .travis/push_other_repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+git config --global user.email "ci@bitprim.org"
+git config --global user.name "Bitprim CI"      
+
+mkdir temp
+cd temp
+
+git clone https://github.com/bitprim/bitprim-node-exe.git --depth 1
+# git clone https://github.com/bitprim/bitprim-node-exe.git
+
+cd bitprim-node-exe
+echo "Travis branch: ${TRAVIS_BRANCH}"
+git checkout ${TRAVIS_BRANCH}
+echo "bitprim-core: $BITPRIM_BUILD_NUMBER" >> versions.txt
+cat versions.txt
+git add . versions.txt
+git commit --message "Travis bitprim-core build: $BITPRIM_BUILD_NUMBER, $TRAVIS_BUILD_NUMBER"
+git remote add origin-commit https://${GH_TOKEN}@github.com/bitprim/bitprim-node-exe.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-commit ${TRAVIS_BRANCH} 
