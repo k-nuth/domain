62e7f98ed930b5effe3b7969e40c7fbc97517e2b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-11-02 18:03:33 -0300

    versioning py-native

--- .travis/push_other_repo.sh
+++ .travis/push_other_repo.sh
@@ -26,7 +26,8 @@ git config --global user.name "Bitprim CI"
 mkdir temp
 cd temp
 
-# git clone https://github.com/bitprim/bitprim-node-exe.git --depth 1
+
+# --------------------------------------------------------------------------------------------------------------------
 git clone https://github.com/bitprim/bitprim-node-exe.git
 
 cd bitprim-node-exe
@@ -40,3 +41,24 @@ git add . versions.txt
 git commit --message "Travis bitprim-core build: $BITPRIM_BUILD_NUMBER, $TRAVIS_BUILD_NUMBER" || true
 git remote add origin-commit https://${GH_TOKEN}@github.com/bitprim/bitprim-node-exe.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-commit ${TRAVIS_BRANCH}  || true
+
+cd ..
+
+# --------------------------------------------------------------------------------------------------------------------
+git clone https://github.com/bitprim/bitprim-py-native.git
+
+cd bitprim-py-native
+echo "Travis branch: ${TRAVIS_BRANCH}"
+git checkout ${TRAVIS_BRANCH}
+
+replace_versions bitprim-core $BITPRIM_BUILD_NUMBER
+
+cat versions.txt
+git add . versions.txt
+git commit --message "Travis bitprim-core build: $BITPRIM_BUILD_NUMBER, $TRAVIS_BUILD_NUMBER" || true
+git remote add origin-commit https://${GH_TOKEN}@github.com/bitprim/bitprim-py-native.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-commit ${TRAVIS_BRANCH}  || true
+
+cd ..
+
+# --------------------------------------------------------------------------------------------------------------------
