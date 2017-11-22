aed826d99ae12c9e90d0dbebc3c4a265e809e314
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-11-22 13:06:58 -0300

    linting features, test

--- src/chain/chain_state.cpp
+++ src/chain/chain_state.cpp
@@ -383,7 +383,7 @@ template <typename T, typename U, typename V, typename R>
 // requires(SameType<T, U> && SameType<U, V> && Domain<R, T>)
 inline constexpr
 auto select_1_3_ac(T&& a, U&& b, V&& c, R r) FN(
-    // precondition: a <= c
+    // precondition: ! r(c, a)
     r(b, a) ?
           std::forward<T>(a)                                      // b, a, c 
         : select_0_2(std::forward<U>(b), std::forward<V>(c), r)   // a is not the median
