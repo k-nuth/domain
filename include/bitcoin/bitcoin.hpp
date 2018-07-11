8d812be798d463561c819d6c96042f029892f670
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-07-11 20:18:22 -0300

    temporary disable inclusion of new headers

--- include/bitcoin/bitcoin.hpp
+++ include/bitcoin/bitcoin.hpp
@@ -46,21 +46,21 @@
 #include <bitcoin/bitcoin/config/checkpoint.hpp>
 #include <bitcoin/bitcoin/config/directory.hpp>
 #include <bitcoin/bitcoin/config/endpoint.hpp>
-#include <bitcoin/bitcoin/config/ec_private.hpp>
-#include <bitcoin/bitcoin/config/endorsement.hpp>
+// #include <bitcoin/bitcoin/config/ec_private.hpp>
+// #include <bitcoin/bitcoin/config/endorsement.hpp>
 #include <bitcoin/bitcoin/config/hash160.hpp>
 #include <bitcoin/bitcoin/config/hash256.hpp>
-#include <bitcoin/bitcoin/config/header.hpp>
-#include <bitcoin/bitcoin/config/input.hpp>
-#include <bitcoin/bitcoin/config/output.hpp>
+// #include <bitcoin/bitcoin/config/header.hpp>
+// #include <bitcoin/bitcoin/config/input.hpp>
+// #include <bitcoin/bitcoin/config/output.hpp>
 #include <bitcoin/bitcoin/config/parameter.hpp>
 #include <bitcoin/bitcoin/config/parser.hpp>
-#include <bitcoin/bitcoin/config/point.hpp>
+// #include <bitcoin/bitcoin/config/point.hpp>
 #include <bitcoin/bitcoin/config/printer.hpp>
-#include <bitcoin/bitcoin/config/script.hpp>
+// #include <bitcoin/bitcoin/config/script.hpp>
 #include <bitcoin/bitcoin/config/settings.hpp>
 #include <bitcoin/bitcoin/config/sodium.hpp>
-#include <bitcoin/bitcoin/config/transaction.hpp>
+// #include <bitcoin/bitcoin/config/transaction.hpp>
 #include <bitcoin/bitcoin/formats/base_10.hpp>
 #include <bitcoin/bitcoin/formats/base_16.hpp>
 #include <bitcoin/bitcoin/formats/base_58.hpp>
