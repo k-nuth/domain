59523fa491002bc09a46e11a0d9f21ca542f2f8d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-22 15:51:11 -0300

    east const for get_data class

--- include/bitcoin/bitcoin/message/get_data.hpp
+++ include/bitcoin/bitcoin/message/get_data.hpp
@@ -95,8 +95,8 @@ class BC_API get_data
     bool operator!=(const get_data& other) const;
 
     static const std::string command;
-    static const uint32_t version_minimum;
-    static const uint32_t version_maximum;
+    static uint32_t const version_minimum;
+    static uint32_t const version_maximum;
 };
 
 }  // namespace message
