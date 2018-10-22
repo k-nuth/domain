2d495d98e65709865b11b71171b045b7900f9000
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-22 12:57:36 -0300

    helpers

--- commiter.py
+++ commiter.py
@@ -2,17 +2,16 @@ import os
 from subprocess import call
 
 files = [
-	# 'chain/block.cpp'
-
-	'chain/header.cpp'
+	'chain/block.cpp'
+	,'chain/header.cpp'
 	, 'chain/input.cpp'
 	, 'chain/output.cpp'
 	, 'chain/output_point.cpp'
-	, 'chain/point.cpp'
+	,'chain/point.cpp'
 	, 'chain/script.cpp'
 	, 'chain/transaction.cpp'
 	, 'chain/witness.cpp'
-	
+
 	# , 'machine/operation.cpp'
 	# , 'message/address.cpp'
 	# , 'message/alert.cpp'
@@ -59,21 +58,29 @@ def process():
         # print(filename.split('/')[-1])
         class_name = filename.split('/')[-1]
 
-        inc = os.path.join('/Users/fernando/dev/bitprim-domain', 'include/bitcoin/bitcoin/', filename) + '.hpp'
-        src = os.path.join('/Users/fernando/dev/bitprim-domain', 'src/', f)
+        inc = os.path.join('include/bitcoin/bitcoin/', filename) + '.hpp'
+        src = os.path.join('src/', f)
         print(inc)
         print(src)
-		
-        ret = call(['clang-format', '-i', '-style=file', inc])
+
+
+        # print('git commit -m "fixes in {} class"'.format(class_name))
+
+        ret = call(["git", "add", inc])
         if ret != 0:
             print('error 1')
             print(inc)
             return
 
-        ret = call(['clang-format', '-i', '-style=file', src])
+        print(call(["git", "add", src]))
         if ret != 0:
             print('error 2')
             print(src)
+            return
+
+        print(call(['git', 'commit', '-m', 'style changes for {} class'.format(class_name)]))
+        if ret != 0:
+            print('error 3')
             return
 
 
