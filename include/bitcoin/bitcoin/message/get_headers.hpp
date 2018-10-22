cd884885493a083d8580b69d144453f546595f79
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-22 15:43:38 -0300

    style changes for get_headers class

--- include/bitcoin/bitcoin/message/get_headers.hpp
+++ include/bitcoin/bitcoin/message/get_headers.hpp
@@ -23,8 +23,8 @@
 #include <memory>
 #include <string>
 
-#include <bitcoin/infrastructure/math/hash.hpp>
 #include <bitcoin/bitcoin/message/get_blocks.hpp>
+#include <bitcoin/infrastructure/math/hash.hpp>
 #include <bitcoin/infrastructure/utility/container_sink.hpp>
 #include <bitcoin/infrastructure/utility/container_source.hpp>
 
@@ -35,18 +35,16 @@ namespace libbitcoin {
 namespace message {
 
 class BC_API get_headers
-  : public get_blocks
-{
-public:
+    : public get_blocks {
+   public:
     typedef std::shared_ptr<get_headers> ptr;
     typedef std::shared_ptr<const get_headers> const_ptr;
 
     static get_headers factory_from_data(uint32_t version, const data_chunk& data);
     static get_headers factory_from_data(uint32_t version, data_source& stream);
-    
+
     template <Reader R, BITPRIM_IS_READER(R)>
-    static get_headers factory_from_data(uint32_t version, R& source)
-    {
+    static get_headers factory_from_data(uint32_t version, R& source) {
         get_headers instance;
         instance.from_data(version, source);
         return instance;
@@ -60,21 +58,21 @@ public:
     get_headers(const get_headers& other);
     get_headers(get_headers&& other);
 
-    bool from_data(uint32_t version, const data_chunk& data); /*override*/ //TODO(fernando): check if this function is used in a run-time-polymorphic way
-    bool from_data(uint32_t version, data_source& stream); /*override*/ //TODO(fernando): check if this function is used in a run-time-polymorphic way
-    
+    bool from_data(uint32_t version, const data_chunk& data); /*override*/  //TODO(fernando): check if this function is used in a run-time-polymorphic way
+    bool from_data(uint32_t version, data_source& stream); /*override*/     //TODO(fernando): check if this function is used in a run-time-polymorphic way
+
     template <Reader R, BITPRIM_IS_READER(R)>
-    bool from_data(uint32_t version, R& source) /*override*/ //TODO(fernando): check if this function is used in a run-time-polymorphic way
+    bool from_data(uint32_t version, R& source) /*override*/  //TODO(fernando): check if this function is used in a run-time-polymorphic way
     {
         if (!get_blocks::from_data(version, source))
             return false;
-    
+
         if (version < get_headers::version_minimum)
             source.invalidate();
-    
+
         if (!source)
             reset();
-    
+
         return source;
     }
 
@@ -92,7 +90,7 @@ public:
     static const uint32_t version_maximum;
 };
 
-} // namespace message
-} // namespace libbitcoin
+}  // namespace message
+}  // namespace libbitcoin
 
 #endif
--- src/message/get_headers.cpp
+++ src/message/get_headers.cpp
@@ -18,8 +18,8 @@
  */
 #include <bitcoin/bitcoin/message/get_headers.hpp>
 
-#include <bitcoin/infrastructure/math/hash.hpp>
 #include <bitcoin/bitcoin/message/version.hpp>
+#include <bitcoin/infrastructure/math/hash.hpp>
 
 namespace libbitcoin {
 namespace message {
@@ -28,15 +28,13 @@ const std::string get_headers::command = "getheaders";
 const uint32_t get_headers::version_minimum = version::level::headers;
 const uint32_t get_headers::version_maximum = version::level::maximum;
 
-get_headers get_headers::factory_from_data(uint32_t version, const data_chunk& data)
-{
+get_headers get_headers::factory_from_data(uint32_t version, const data_chunk& data) {
     get_headers instance;
     instance.from_data(version, data);
     return instance;
 }
 
-get_headers get_headers::factory_from_data(uint32_t version, data_source& stream)
-{
+get_headers get_headers::factory_from_data(uint32_t version, data_source& stream) {
     get_headers instance;
     instance.from_data(version, stream);
     return instance;
@@ -50,37 +48,30 @@ get_headers get_headers::factory_from_data(uint32_t version, data_source& stream
 //}
 
 get_headers::get_headers()
-  : get_blocks()
-{
+    : get_blocks() {
 }
 
 get_headers::get_headers(const hash_list& start, const hash_digest& stop)
-  : get_blocks(start, stop)
-{
+    : get_blocks(start, stop) {
 }
 
 get_headers::get_headers(hash_list&& start, hash_digest&& stop)
-  : get_headers(start, stop)
-{
+    : get_headers(start, stop) {
 }
 
 get_headers::get_headers(const get_headers& other)
-  : get_blocks(other)
-{
+    : get_blocks(other) {
 }
 
 get_headers::get_headers(get_headers&& other)
-  : get_blocks(other)
-{
+    : get_blocks(other) {
 }
 
-bool get_headers::from_data(uint32_t version, const data_chunk& data)
-{
+bool get_headers::from_data(uint32_t version, const data_chunk& data) {
     return get_blocks::from_data(version, data);
 }
 
-bool get_headers::from_data(uint32_t version, data_source& stream)
-{
+bool get_headers::from_data(uint32_t version, data_source& stream) {
     return get_blocks::from_data(version, stream);
 }
 
@@ -98,22 +89,19 @@ bool get_headers::from_data(uint32_t version, data_source& stream)
 //    return source;
 //}
 
-get_headers& get_headers::operator=(get_headers&& other)
-{
+get_headers& get_headers::operator=(get_headers&& other) {
     set_start_hashes(other.start_hashes());
     set_stop_hash(other.stop_hash());
     return *this;
 }
 
-bool get_headers::operator==(const get_headers& other) const
-{
+bool get_headers::operator==(const get_headers& other) const {
     return (static_cast<get_blocks>(*this) == static_cast<get_blocks>(other));
 }
 
-bool get_headers::operator!=(const get_headers& other) const
-{
+bool get_headers::operator!=(const get_headers& other) const {
     return (static_cast<get_blocks>(*this) != static_cast<get_blocks>(other));
 }
 
-} // namespace message
-} // namespace libbitcoin
+}  // namespace message
+}  // namespace libbitcoin
