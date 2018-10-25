632010739e3537e8869ebae105f3cf808323a9a1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-25 18:18:01 -0300

    canonical C++ way for pong class

--- src/message/pong.cpp
+++ src/message/pong.cpp
@@ -43,7 +43,7 @@ pong pong::factory_from_data(uint32_t version, data_source& stream) {
     return instance;
 }
 
-size_t pong::satoshi_fixed_size(uint32_t version) {
+size_t pong::satoshi_fixed_size(uint32_t /*version*/) {
     return sizeof(nonce_);
 }
 
