1829b6bb2d6287b78f867a77a6bc4b9ce3255fd7
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-02-01 07:24:39 -0300

    appveyor changes

---
+++ src/keoken/message/base.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2016-2020 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <kth/domain/keoken/message/base.hpp>
+
+#include <kth/infrastructure/utility/ostream_writer.hpp>
+
+namespace kth {
+namespace keoken {
+namespace message {
+namespace base {
+
+size_t serialized_size() {
+    return sizeof(uint16_t) + sizeof(uint16_t);
+}
+
+// void to_data(bc::writer& sink, uint16_t version, uint16_t type) {
+//     sink.write_2_bytes_big_endian(version);
+//     sink.write_2_bytes_big_endian(type);
+// }
+
+}  // namespace base
+}  // namespace message
+}  // namespace keoken
+}  // namespace kth
