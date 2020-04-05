937ea640302cae26929cecbb6d12bcb883c8c6bd
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-05 19:14:18 -0300

    input_basis uses Catch2

--- include/kth/domain/chain/input_basis.hpp
+++ include/kth/domain/chain/input_basis.hpp
@@ -15,7 +15,10 @@
 #include <kth/domain/chain/script.hpp>
 #include <kth/domain/chain/witness.hpp>
 #include <kth/domain/define.hpp>
+#include <kth/domain/multi_crypto_settings.hpp>
+
 // #include <kth/domain/wallet/payment_address.hpp>
+
 #include <kth/infrastructure/math/hash.hpp>
 #include <kth/infrastructure/utility/container_sink.hpp>
 #include <kth/infrastructure/utility/container_source.hpp>
