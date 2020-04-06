5afa8368d72c0098dc32acf1f075dd7c7906d7fc
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-06 20:40:00 -0300

    memory_pool uses Catch2

--- test/message/memory_pool.cpp
+++ test/message/memory_pool.cpp
@@ -2,48 +2,45 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <kth/domain.hpp>
-#include <boost/test/unit_test.hpp>
+#include <test_helpers.hpp>
 
 using namespace kth;
 using namespace kd;
 
-BOOST_AUTO_TEST_SUITE(memory_pool_tests)
+// Start Boost Suite: memory pool tests
 
-BOOST_AUTO_TEST_CASE(from_data_insufficient_version_failure) {
+TEST_CASE("memory pool - from data insufficient version failure", "[memory pool]") {
     const message::memory_pool expected;
     data_chunk const data = expected.to_data(message::version::level::maximum);
     message::memory_pool instance{};
-
-    BOOST_REQUIRE_EQUAL(false, entity_from_data(instance, 
-                                   message::memory_pool::version_minimum - 1, data));
-    BOOST_REQUIRE_EQUAL(false, instance.is_valid());
+    REQUIRE( ! entity_from_data(instance, message::memory_pool::version_minimum - 1, data));
+    REQUIRE( ! instance.is_valid());
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk) {
+TEST_CASE("memory pool - roundtrip to data factory from data chunk", "[memory pool]") {
     const message::memory_pool expected{};
     auto const data = expected.to_data(message::version::level::maximum);
     auto const result = create<message::memory_pool>(
         message::version::level::maximum, data);
 
-    BOOST_REQUIRE_EQUAL(0u, data.size());
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(0u, result.serialized_size(message::version::level::maximum));
+    REQUIRE(0u == data.size());
+    REQUIRE(result.is_valid());
+    REQUIRE(0u == result.serialized_size(message::version::level::maximum));
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream) {
+TEST_CASE("memory pool - roundtrip to data factory from data stream", "[memory pool]") {
     const message::memory_pool expected{};
     auto const data = expected.to_data(message::version::level::maximum);
     data_source istream(data);
     auto const result = create<message::memory_pool>(
         message::version::level::maximum, istream);
 
-    BOOST_REQUIRE_EQUAL(0u, data.size());
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(0u, result.serialized_size(message::version::level::maximum));
+    REQUIRE(0u == data.size());
+    REQUIRE(result.is_valid());
+    REQUIRE(0u == result.serialized_size(message::version::level::maximum));
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_reader) {
+TEST_CASE("memory pool - roundtrip to data factory from data reader", "[memory pool]") {
     const message::memory_pool expected{};
     auto const data = expected.to_data(message::version::level::maximum);
     data_source istream(data);
