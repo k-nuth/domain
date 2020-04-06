4f1a127c5e0c34ba59fdc8142a19ef4b434fdb52
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-06 20:14:00 -0300

    Using modern libraries in filter_clear

--- test/message/filter_clear.cpp
+++ test/message/filter_clear.cpp
@@ -2,55 +2,54 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <kth/domain.hpp>
-#include <boost/test/unit_test.hpp>
+#include <test_helpers.hpp>
 
 using namespace kth;
 using namespace kd;
 using namespace kth::domain::message;
 
-BOOST_AUTO_TEST_SUITE(filter_clear_tests)
+// Start Boost Suite: filter clear tests
 
-BOOST_AUTO_TEST_CASE(from_data_insufficient_version_failure) {
+TEST_CASE("filter clear - from data insufficient version failure", "[filter clear]") {
     static const filter_clear expected{};
     auto const raw = expected.to_data(version::level::maximum);
     filter_clear instance{};
 
-    BOOST_REQUIRE(!entity_from_data(instance, filter_clear::version_minimum - 1, raw));
-    BOOST_REQUIRE(!instance.is_valid());
+    REQUIRE(!entity_from_data(instance, filter_clear::version_minimum - 1, raw));
+    REQUIRE(!instance.is_valid());
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk) {
+TEST_CASE("filter clear - roundtrip to data factory from data chunk", "[filter clear]") {
     static const filter_clear expected{};
     auto const data = expected.to_data(version::level::maximum);
     auto const result = create<filter_clear>(version::level::maximum, data);
 
-    BOOST_REQUIRE_EQUAL(data.size(), 0u);
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(result.serialized_size(version::level::maximum), 0u);
+    REQUIRE(data.size() == 0u);
+    REQUIRE(result.is_valid());
+    REQUIRE(result.serialized_size(version::level::maximum) == 0u);
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream) {
+TEST_CASE("filter clear - roundtrip to data factory from data stream", "[filter clear]") {
     static const filter_clear expected{};
     auto const data = expected.to_data(version::level::maximum);
     data_source istream(data);
     auto const result = create<filter_clear>(version::level::maximum, istream);
 
-    BOOST_REQUIRE_EQUAL(data.size(), 0u);
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(result.serialized_size(version::level::maximum), 0u);
+    REQUIRE(data.size() == 0u);
+    REQUIRE(result.is_valid());
+    REQUIRE(result.serialized_size(version::level::maximum) == 0u);
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_reader) {
+TEST_CASE("filter clear - roundtrip to data factory from data reader", "[filter clear]") {
     static const filter_clear expected{};
     auto const data = expected.to_data(version::level::maximum);
     data_source istream(data);
     istream_reader source(istream);
     auto const result = create<filter_clear>(version::level::maximum, source);
 
-    BOOST_REQUIRE_EQUAL(data.size(), 0u);
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(result.serialized_size(version::level::maximum), 0u);
+    REQUIRE(data.size() == 0u);
+    REQUIRE(result.is_valid());
+    REQUIRE(result.serialized_size(version::level::maximum) == 0u);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// End Boost Suite
