b4d545ebc54f68b31eccfe369d67839560f444aa
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-06 20:20:00 -0300

    use Catch2 in get_address

--- test/message/get_address.cpp
+++ test/message/get_address.cpp
@@ -2,38 +2,37 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <kth/domain.hpp>
-#include <boost/test/unit_test.hpp>
+#include <test_helpers.hpp>
 
 using namespace kth;
 using namespace kd;
 
-BOOST_AUTO_TEST_SUITE(get_address_tests)
+// Start Boost Suite: get address tests
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk) {
+TEST_CASE("get address - roundtrip to data factory from data chunk", "[get address]") {
     const message::get_address expected{};
     auto const data = expected.to_data(message::version::level::minimum);
     auto const result = create<message::get_address>(
         message::version::level::minimum, data);
 
-    BOOST_REQUIRE_EQUAL(0u, data.size());
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(0u, result.serialized_size(message::version::level::minimum));
+    REQUIRE(0u == data.size());
+    REQUIRE(result.is_valid());
+    REQUIRE(0u == result.serialized_size(message::version::level::minimum));
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream) {
+TEST_CASE("get address - roundtrip to data factory from data stream", "[get address]") {
     const message::get_address expected{};
     auto const data = expected.to_data(message::version::level::minimum);
     data_source istream(data);
     auto const result = create<message::get_address>(
         message::version::level::minimum, istream);
 
-    BOOST_REQUIRE_EQUAL(0u, data.size());
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(0u, result.serialized_size(message::version::level::minimum));
+    REQUIRE(0u == data.size());
+    REQUIRE(result.is_valid());
+    REQUIRE(0u == result.serialized_size(message::version::level::minimum));
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_reader) {
+TEST_CASE("get address - roundtrip to data factory from data reader", "[get address]") {
     const message::get_address expected{};
     auto const data = expected.to_data(message::version::level::minimum);
     data_source istream(data);
