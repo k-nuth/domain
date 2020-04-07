ee2787310498d61895cf3357db3e0cfbdc2a4f2b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-06 21:52:00 -0300

    Never use Boost.Test again transaction

--- src/config/transaction.cpp
+++ src/config/transaction.cpp
@@ -43,7 +43,7 @@ std::istream& operator>>(std::istream& input, transaction& argument) {
     std::string hexcode;
     input >> hexcode;
 
-    if ( ! argument.value_.from_data(base16(hexcode))) {
+    if ( ! entity_from_data(argument.value_, base16(hexcode))) {
         BOOST_THROW_EXCEPTION(invalid_option_value(hexcode));
     }
 
