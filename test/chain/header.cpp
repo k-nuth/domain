af41f8b9462970f52341be72552a966ebc66c866
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-06 23:41:00 -0300

    Never use Boost.Test again header

--- test/chain/header.cpp
+++ test/chain/header.cpp
@@ -45,16 +45,16 @@ TEST_CASE("chain header  constructor 3  always  equals params", "[chain header]"
     auto merkle = hash_literal("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
 
     chain::header instance(version, std::move(previous), std::move(merkle), timestamp, bits, nonce);
-    BOOST_REQUIRE(instance.is_valid());
-    BOOST_REQUIRE_EQUAL(version, instance.version());
-    BOOST_REQUIRE_EQUAL(timestamp, instance.timestamp());
-    BOOST_REQUIRE_EQUAL(bits, instance.bits());
-    BOOST_REQUIRE_EQUAL(nonce, instance.nonce());
-    BOOST_REQUIRE(previous == instance.previous_block_hash());
-    BOOST_REQUIRE(merkle == instance.merkle());
+    REQUIRE(instance.is_valid());
+    REQUIRE(version == instance.version());
+    REQUIRE(timestamp == instance.timestamp());
+    REQUIRE(bits == instance.bits());
+    REQUIRE(nonce == instance.nonce());
+    REQUIRE(previous == instance.previous_block_hash());
+    REQUIRE(merkle == instance.merkle());
 }
 
-BOOST_AUTO_TEST_CASE(header__constructor_4__always__equals_params) {
+TEST_CASE("chain header  constructor 4  always  equals params", "[chain header]") {
     chain::header const expected(
         10u,
         hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
