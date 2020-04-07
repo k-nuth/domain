bcfd9a9852ada03502771b942526208c2972b8c3
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-06 21:02:00 -0300

    Never use Boost.Test again verack

--- test/message/verack.cpp
+++ test/message/verack.cpp
@@ -2,38 +2,37 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <kth/domain.hpp>
-#include <boost/test/unit_test.hpp>
+#include <test_helpers.hpp>
 
 using namespace kth;
 using namespace kd;
 
-BOOST_AUTO_TEST_SUITE(verack_tests)
+// Start Boost Suite: verack tests
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk) {
+TEST_CASE("verack - roundtrip to data factory from data chunk", "[verack]") {
     const message::verack expected{};
     auto const data = expected.to_data(message::version::level::minimum);
     auto const result = create<message::verack>(
         message::version::level::minimum, data);
 
-    BOOST_REQUIRE_EQUAL(0u, data.size());
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(0u, result.serialized_size(message::version::level::minimum));
+    REQUIRE(0u == data.size());
+    REQUIRE(result.is_valid());
+    REQUIRE(0u == result.serialized_size(message::version::level::minimum));
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream) {
+TEST_CASE("verack - roundtrip to data factory from data stream", "[verack]") {
     const message::verack expected{};
     auto const data = expected.to_data(message::version::level::minimum);
     data_source istream(data);
     auto const result = create<message::verack>(
         message::version::level::minimum, istream);
 
-    BOOST_REQUIRE_EQUAL(0u, data.size());
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE_EQUAL(0u, result.serialized_size(message::version::level::minimum));
+    REQUIRE(0u == data.size());
+    REQUIRE(result.is_valid());
+    REQUIRE(0u == result.serialized_size(message::version::level::minimum));
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_reader) {
+TEST_CASE("verack - roundtrip to data factory from data reader", "[verack]") {
     const message::verack expected{};
     auto const data = expected.to_data(message::version::level::minimum);
     data_source istream(data);
