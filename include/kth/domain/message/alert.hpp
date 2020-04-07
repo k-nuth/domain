e381b639959259069fbf5e001fa38faa12b13401
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-07 19:10:00 -0300

    Never use Boost.Test again alert

--- include/kth/domain/message/alert.hpp
+++ include/kth/domain/message/alert.hpp
@@ -55,9 +55,6 @@ public:
     void set_signature(data_chunk const& value);
     void set_signature(data_chunk&& value);
 
-    // bool from_data(uint32_t version, data_chunk const& data);
-    // bool from_data(uint32_t version, std::istream& stream);
-
     template <typename R, KTH_IS_READER(R)>
     bool from_data(uint32_t /*version*/, R& source) {
         reset();
