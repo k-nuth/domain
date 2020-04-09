28860501fd105ad38c2cf761c6549549d812a853
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-08 22:50:00 -0300

    use Catch2 in send_compact

--- include/kth/domain/message/send_compact.hpp
+++ include/kth/domain/message/send_compact.hpp
@@ -52,9 +52,6 @@ public:
     
     void set_version(uint64_t version);
 
-    // bool from_data(uint32_t version, data_chunk const& data);
-    // bool from_data(uint32_t version, std::istream& stream);
-
     template <typename R, KTH_IS_READER(R)>
     bool from_data(uint32_t version, R& source) {
         reset();
