32c98d87b505143897a702d19c4a01a0bebe727e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-08 22:26:00 -0300

    use Catch2 in header

--- test/message/header.cpp
+++ test/message/header.cpp
@@ -224,17 +224,17 @@ TEST_CASE("message header  operator assign equals 2  always  matches equivalent"
         6523454u,
         68644u);
 
-    BOOST_REQUIRE(value.is_valid());
+    REQUIRE(value.is_valid());
 
     message::header instance;
-    BOOST_REQUIRE(!instance.is_valid());
+    REQUIRE(!instance.is_valid());
 
     instance = std::move(value);
-    BOOST_REQUIRE(instance.is_valid());
-    BOOST_REQUIRE(value == instance);
+    REQUIRE(instance.is_valid());
+    REQUIRE(value == instance);
 }
 
-BOOST_AUTO_TEST_CASE(header__operator_boolean_equals_1__duplicates__returns_true) {
+TEST_CASE("message header  operator boolean equals 1  duplicates  returns true", "[message header]") {
     chain::header const expected(
         10u,
         hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
