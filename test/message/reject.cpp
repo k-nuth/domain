a4aa193abfb1ea4a7f0ecd44364b1cfb86733664
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-08 23:05:00 -0300

    Remove Boost testing framework in reject

--- test/message/reject.cpp
+++ test/message/reject.cpp
@@ -252,13 +252,13 @@ TEST_CASE("reject  factory from data 2  valid input  success", "[reject]") {
     auto const data = expected.to_data(version_maximum);
     data_source istream(data);
     auto const result = create<message::reject>(version_maximum, istream);
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE(expected == result);
-    BOOST_REQUIRE_EQUAL(data.size(), result.serialized_size(version_maximum));
-    BOOST_REQUIRE_EQUAL(expected.serialized_size(version_maximum), result.serialized_size(version_maximum));
+    REQUIRE(result.is_valid());
+    REQUIRE(expected == result);
+    REQUIRE(data.size() == result.serialized_size(version_maximum));
+    REQUIRE(expected.serialized_size(version_maximum) == result.serialized_size(version_maximum));
 }
 
-BOOST_AUTO_TEST_CASE(reject__factory_from_data_3__valid_input__success) {
+TEST_CASE("reject  factory from data 3  valid input  success", "[reject]") {
     const message::reject expected(
         message::reject::reason_code::duplicate,
         message::transaction::command,
