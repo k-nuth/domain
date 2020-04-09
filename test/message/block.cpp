ef3cf5682b01dd24cc5b5c0f0868f30662c1421d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-08 23:23:00 -0300

    Remove Boost testing framework in block

--- test/message/block.cpp
+++ test/message/block.cpp
@@ -362,10 +362,10 @@ TEST_CASE("block  operator boolean equals 2  differs  returns false", "[message 
          chain::transaction(4, 16, {}, {})});
 
     message::block instance;
-    BOOST_REQUIRE_EQUAL(false, instance == expected);
+    REQUIRE(instance != expected);
 }
 
-BOOST_AUTO_TEST_CASE(block__operator_boolean_not_equals_2__duplicates__returns_false) {
+TEST_CASE("block  operator boolean not equals 2  duplicates  returns false", "[message block]") {
     const message::block expected(
         chain::header(10u,
                       hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
