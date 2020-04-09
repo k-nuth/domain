e53859df0331696ada1b0a5a926b2d2c4fcb9d85
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-09 19:10:00 -0300

    use Catch2 in reject

--- test/message/reject.cpp
+++ test/message/reject.cpp
@@ -424,10 +424,10 @@ TEST_CASE("reject  operator boolean equals  duplicates  returns true", "[reject]
         hash_literal("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
     message::reject instance(expected);
-    BOOST_REQUIRE(instance == expected);
+    REQUIRE(instance == expected);
 }
 
-BOOST_AUTO_TEST_CASE(reject__operator_boolean_equals__differs__returns_false) {
+TEST_CASE("reject  operator boolean equals  differs  returns false", "[reject]") {
     const message::reject expected(
         message::reject::reason_code::dust,
         "My Message",
