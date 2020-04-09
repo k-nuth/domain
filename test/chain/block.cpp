9d98b6b2fd21eeca9c40da91cf93924d91756a95
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-09 19:47:00 -0300

    use Catch2 framework in block

--- test/chain/block.cpp
+++ test/chain/block.cpp
@@ -515,10 +515,10 @@ TEST_CASE("block  operator boolean not equals  duplicates  returns false", "[blo
          chain::transaction(4, 16, {}, {})});
 
     chain::block instance(expected);
-    BOOST_REQUIRE(!(instance != expected));
+    REQUIRE(!(instance != expected));
 }
 
-BOOST_AUTO_TEST_CASE(block__operator_boolean_not_equals__differs__returns_true) {
+TEST_CASE("block  operator boolean not equals  differs  returns true", "[block generate merkle root]") {
     const chain::block expected(
         chain::header(10u,
                       hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
