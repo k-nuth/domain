20e1522224ed3a2c2e94bb989729c254eb0e6c41
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-09 20:22:00 -0300

    Never use Boost.Test again header

--- test/message/header.cpp
+++ test/message/header.cpp
@@ -309,10 +309,10 @@ TEST_CASE("message header  operator boolean equals 2  differs  returns false", "
         68644u);
 
     message::header instance;
-    BOOST_REQUIRE_EQUAL(false, instance == expected);
+    REQUIRE(instance != expected);
 }
 
-BOOST_AUTO_TEST_CASE(header__operator_boolean_not_equals_2__duplicates__returns_false) {
+TEST_CASE("message header  operator boolean not equals 2  duplicates  returns false", "[message header]") {
     const message::header expected(
         10u,
         hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
