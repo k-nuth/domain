4d4b7951418f6c0d97f805f4879f927b812bd298
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-15 19:44:00 -0300

    Never use Boost.Test again stealth_address

--- test/wallet/stealth_address.cpp
+++ test/wallet/stealth_address.cpp
@@ -2,67 +2,66 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <kth/domain.hpp>
-#include <boost/test/unit_test.hpp>
+#include <test_helpers.hpp>
 
 using namespace kth;
 using namespace kd;
 using namespace kth::domain::wallet;
 
-BOOST_AUTO_TEST_SUITE(stealth_address_tests)
+// Start Boost Suite: stealth address tests
 
 #define SCAN_KEY "03d9e876028f4fc062c19f7097762e4affc2ce4edfffa7d42e3c17cd157ec6d1bc"
 #define SPEND_KEY1 "0215a49b55a2ed2a02569cb6c018644211d408caab3aca86d2cc7d6a9e5789b1d2"
 #define STEALTH_ADDRESS "vJmzLu29obZcUGXXgotapfQLUpz7dfnZpbr4xg1R75qctf8xaXAteRdi3ZUk3T2ZMSad5KyPbve7uyH6eswYAxLHRVSbWgNUeoGuXp"
 
-BOOST_AUTO_TEST_CASE(stealth_address__construct__string__expected_encoding) {
+TEST_CASE("stealth address  construct  string  expected encoding", "[stealth address]") {
     ec_compressed scan;
     ec_compressed spend1;
-    BOOST_REQUIRE(decode_base16(scan, SCAN_KEY));
-    BOOST_REQUIRE(decode_base16(spend1, SPEND_KEY1));
+    REQUIRE(decode_base16(scan, SCAN_KEY));
+    REQUIRE(decode_base16(spend1, SPEND_KEY1));
     stealth_address address({}, scan, {spend1}, 0, 42);
-    BOOST_REQUIRE(address);
-    BOOST_REQUIRE_EQUAL(address.encoded(), STEALTH_ADDRESS);
+    REQUIRE(address);
+    REQUIRE(address.encoded() == STEALTH_ADDRESS);
 }
 
-BOOST_AUTO_TEST_CASE(stealth_address__construct__decoded__expected_properties) {
+TEST_CASE("stealth address  construct  decoded  expected properties", "[stealth address]") {
     stealth_address address(STEALTH_ADDRESS);
-    BOOST_REQUIRE(address);
-    BOOST_REQUIRE_EQUAL((size_t)address.version(), 42);
-    BOOST_REQUIRE_EQUAL(encode_base16(address.scan_key()), SCAN_KEY);
-    BOOST_REQUIRE_EQUAL(address.spend_keys().size(), 1u);
-    BOOST_REQUIRE_EQUAL(encode_base16(address.spend_keys()[0]), SPEND_KEY1);
-    BOOST_REQUIRE_EQUAL((size_t)address.signatures(), 1u);
-    BOOST_REQUIRE_EQUAL(address.filter().size(), 0u);
-    BOOST_REQUIRE_EQUAL(address.encoded(), STEALTH_ADDRESS);
+    REQUIRE(address);
+    REQUIRE((size_t)address.version() == 42);
+    REQUIRE(encode_base16(address.scan_key()) == SCAN_KEY);
+    REQUIRE(address.spend_keys().size() == 1u);
+    REQUIRE(encode_base16(address.spend_keys()[0]) == SPEND_KEY1);
+    REQUIRE((size_t)address.signatures() == 1u);
+    REQUIRE(address.filter().size() == 0u);
+    REQUIRE(address.encoded() == STEALTH_ADDRESS);
 }
 
-BOOST_AUTO_TEST_CASE(stealth_address__encoding__scan_mainnet__round_trips) {
+TEST_CASE("stealth address  encoding  scan mainnet  round trips", "[stealth address]") {
     auto const encoded = "vJmzLu29obZcUGXXgotapfQLUpz7dfnZpbr4xg1R75qctf8xaXAteRdi3ZUk3T2ZMSad5KyPbve7uyH6eswYAxLHRVSbWgNUeoGuXp";
     stealth_address address(encoded);
-    BOOST_REQUIRE_EQUAL(address.encoded(), encoded);
-    BOOST_REQUIRE_EQUAL(address.version(), 42u);
+    REQUIRE(address.encoded() == encoded);
+    REQUIRE(address.version() == 42u);
 }
 
-BOOST_AUTO_TEST_CASE(stealth_address__encoding__scan_testnet__round_trips) {
+TEST_CASE("stealth address  encoding  scan testnet  round trips", "[stealth address]") {
     std::string const encoded = "waPXhQwQE9tDugfgLkvpDs3dnkPx1RsfDjFt4zBq7EeWeATRHpyQpYrFZR8T4BQy91Vpvshm2TDER8b9ZryuZ8VSzz8ywzNzX8NqF4";
     stealth_address address(encoded);
-    BOOST_REQUIRE_EQUAL(address.encoded(), encoded);
-    BOOST_REQUIRE_EQUAL(address.version(), 43u);
+    REQUIRE(address.encoded() == encoded);
+    REQUIRE(address.version() == 43u);
 }
 
-BOOST_AUTO_TEST_CASE(stealth_address__encoding__scan_pub_mainnet__round_trips) {
+TEST_CASE("stealth address  encoding  scan pub mainnet  round trips", "[stealth address]") {
     auto const encoded = "hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i";
     stealth_address address(encoded);
-    BOOST_REQUIRE_EQUAL(address.encoded(), encoded);
-    BOOST_REQUIRE_EQUAL(address.version(), 42u);
+    REQUIRE(address.encoded() == encoded);
+    REQUIRE(address.version() == 42u);
 }
 
-BOOST_AUTO_TEST_CASE(stealth_address__encoding__scan_pub_testnet__round_trip) {
+TEST_CASE("stealth address  encoding  scan pub testnet  round trip", "[stealth address]") {
     auto const encoded = "idPayBqZUpZH7Y5GTaoEyGxDsEmU377JUmhtqG8yoHCkfGfhnAHmGUJbL";
     stealth_address address(encoded);
-    BOOST_REQUIRE_EQUAL(address.encoded(), encoded);
-    BOOST_REQUIRE_EQUAL(address.version(), 43u);
+    REQUIRE(address.encoded() == encoded);
+    REQUIRE(address.version() == 43u);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// End Boost Suite
