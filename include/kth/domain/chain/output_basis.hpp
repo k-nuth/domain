fdfb6388b8d54ebd46c625d2f93041b57d510e30
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-15 22:26:00 -0300

    output_basis uses Catch2

--- include/kth/domain/chain/output_basis.hpp
+++ include/kth/domain/chain/output_basis.hpp
@@ -54,9 +54,6 @@ public:
     // Deserialization.
     //-------------------------------------------------------------------------
 
-    // bool from_data(data_chunk const& data, bool wire = true);
-    // bool from_data(std::istream& stream, bool wire = true);
-
     template <typename R, KTH_IS_READER(R)>
     bool from_data(R& source, bool /*wire*/ = true, bool /*witness*/ = false) {
         reset();
