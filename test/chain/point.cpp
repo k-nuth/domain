991ca9e73d9b553b20153abaf33d3697efdecf2e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-22 21:01:00 -0300

    use Catch2 in point

--- test/chain/point.cpp
+++ test/chain/point.cpp
@@ -83,180 +83,180 @@ TEST_CASE("point  from data  roundtrip  success", "[point]") {
 
     chain::point initial{hash, index};
 
-    BOOST_REQUIRE(initial.is_valid());
-    BOOST_REQUIRE(hash == initial.hash());
-    BOOST_REQUIRE(index == initial.index());
+    REQUIRE(initial.is_valid());
+    REQUIRE(hash == initial.hash());
+    REQUIRE(index == initial.index());
 
     chain::point point;
 
-    BOOST_REQUIRE(point != initial);
-    BOOST_REQUIRE(entity_from_data(point, initial.to_data()));
-    BOOST_REQUIRE(point.is_valid());
-    BOOST_REQUIRE(point == initial);
+    REQUIRE(point != initial);
+    REQUIRE(entity_from_data(point, initial.to_data()));
+    REQUIRE(point.is_valid());
+    REQUIRE(point == initial);
 }
 
-BOOST_AUTO_TEST_CASE(point__factory_from_data_1__roundtrip__success) {
+TEST_CASE("point  factory from data 1  roundtrip  success", "[point]") {
     auto const raw = to_chunk(base16_literal("46682488f0a721124a3905a1bb72445bf13493e2cd46c5c0c8db1c15afa0d58e00000000"));
     auto const data = data_chunk{
         0x46, 0x68, 0x24, 0x88, 0xf0, 0xa7, 0x21, 0x12, 0x4a, 0x39, 0x05, 0xa1,
         0xbb, 0x72, 0x44, 0x5b, 0xf1, 0x34, 0x93, 0xe2, 0xcd, 0x46, 0xc5, 0xc0,
         0xc8, 0xdb, 0x1c, 0x15, 0xaf, 0xa0, 0xd5, 0x8e, 0x00, 0x00, 0x00, 0x00};
 
-    BOOST_REQUIRE(raw == data);
+    REQUIRE(raw == data);
 
     auto point = create<chain::point>(raw);
 
-    BOOST_REQUIRE(point.is_valid());
-    BOOST_REQUIRE_EQUAL(encode_hash(point.hash()), "8ed5a0af151cdbc8c0c546cde29334f15b4472bba105394a1221a7f088246846");
-    BOOST_REQUIRE_EQUAL(point.index(), 0);
+    REQUIRE(point.is_valid());
+    REQUIRE(encode_hash(point.hash()) == "8ed5a0af151cdbc8c0c546cde29334f15b4472bba105394a1221a7f088246846");
+    REQUIRE(point.index() == 0);
 
     data_chunk output = point.to_data();
-    BOOST_REQUIRE(output == raw);
+    REQUIRE(output == raw);
 }
 
-BOOST_AUTO_TEST_CASE(point__factory_from_data_2__roundtrip__success) {
+TEST_CASE("point  factory from data 2  roundtrip  success", "[point]") {
     auto const raw = to_chunk(base16_literal("46682488f0a721124a3905a1bb72445bf13493e2cd46c5c0c8db1c15afa0d58e00000000"));
     data_chunk const data{
         0x46, 0x68, 0x24, 0x88, 0xf0, 0xa7, 0x21, 0x12, 0x4a, 0x39, 0x05, 0xa1,
         0xbb, 0x72, 0x44, 0x5b, 0xf1, 0x34, 0x93, 0xe2, 0xcd, 0x46, 0xc5, 0xc0,
         0xc8, 0xdb, 0x1c, 0x15, 0xaf, 0xa0, 0xd5, 0x8e, 0x00, 0x00, 0x00, 0x00};
 
-    BOOST_REQUIRE(raw == data);
+    REQUIRE(raw == data);
 
     data_source istream(raw);
     auto point = create<chain::point>(istream);
 
-    BOOST_REQUIRE(point.is_valid());
-    BOOST_REQUIRE_EQUAL(encode_hash(point.hash()), "8ed5a0af151cdbc8c0c546cde29334f15b4472bba105394a1221a7f088246846");
-    BOOST_REQUIRE_EQUAL(point.index(), 0);
+    REQUIRE(point.is_valid());
+    REQUIRE(encode_hash(point.hash()) == "8ed5a0af151cdbc8c0c546cde29334f15b4472bba105394a1221a7f088246846");
+    REQUIRE(point.index() == 0);
 
     data_chunk output = point.to_data();
-    BOOST_REQUIRE(output == raw);
+    REQUIRE(output == raw);
 }
 
-BOOST_AUTO_TEST_CASE(point__factory_from_data_3__roundtrip__success) {
+TEST_CASE("point  factory from data 3  roundtrip  success", "[point]") {
     auto const raw = to_chunk(base16_literal("46682488f0a721124a3905a1bb72445bf13493e2cd46c5c0c8db1c15afa0d58e00000000"));
     data_chunk const data{
         0x46, 0x68, 0x24, 0x88, 0xf0, 0xa7, 0x21, 0x12, 0x4a, 0x39, 0x05, 0xa1,
         0xbb, 0x72, 0x44, 0x5b, 0xf1, 0x34, 0x93, 0xe2, 0xcd, 0x46, 0xc5, 0xc0,
         0xc8, 0xdb, 0x1c, 0x15, 0xaf, 0xa0, 0xd5, 0x8e, 0x00, 0x00, 0x00, 0x00};
 
-    BOOST_REQUIRE(raw == data);
+    REQUIRE(raw == data);
 
     data_source istream(raw);
     istream_reader source(istream);
     auto point = create<chain::point>(source);
 
-    BOOST_REQUIRE(point.is_valid());
-    BOOST_REQUIRE_EQUAL(encode_hash(point.hash()), "8ed5a0af151cdbc8c0c546cde29334f15b4472bba105394a1221a7f088246846");
-    BOOST_REQUIRE_EQUAL(point.index(), 0);
+    REQUIRE(point.is_valid());
+    REQUIRE(encode_hash(point.hash()) == "8ed5a0af151cdbc8c0c546cde29334f15b4472bba105394a1221a7f088246846");
+    REQUIRE(point.index() == 0);
 
     data_chunk output = point.to_data();
-    BOOST_REQUIRE(output == raw);
+    REQUIRE(output == raw);
 }
 
-BOOST_AUTO_TEST_CASE(point__hash_setter_1__roundtrip__success) {
+TEST_CASE("point  hash setter 1  roundtrip  success", "[point]") {
     auto const value = hash_literal("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
 
     chain::point instance;
-    BOOST_REQUIRE(value != instance.hash());
+    REQUIRE(value != instance.hash());
     instance.set_hash(value);
-    BOOST_REQUIRE(value == instance.hash());
+    REQUIRE(value == instance.hash());
 }
 
-BOOST_AUTO_TEST_CASE(point__hash_setter_2__roundtrip__success) {
+TEST_CASE("point  hash setter 2  roundtrip  success", "[point]") {
     auto const value = hash_literal("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
 
     // This must be non-const.
     auto dup_value = value;
 
     chain::point instance;
-    BOOST_REQUIRE(value != instance.hash());
+    REQUIRE(value != instance.hash());
     instance.set_hash(std::move(dup_value));
-    BOOST_REQUIRE(value == instance.hash());
+    REQUIRE(value == instance.hash());
 }
 
-BOOST_AUTO_TEST_CASE(point__index__roundtrip__success) {
+TEST_CASE("point  index  roundtrip  success", "[point]") {
     uint32_t value = 1254u;
     chain::point instance;
-    BOOST_REQUIRE(value != instance.index());
+    REQUIRE(value != instance.index());
     instance.set_index(value);
-    BOOST_REQUIRE_EQUAL(value, instance.index());
+    REQUIRE(value == instance.index());
 }
 
-BOOST_AUTO_TEST_CASE(point__operator_assign_equals_1__always__matches_equivalent) {
+TEST_CASE("point  operator assign equals 1  always  matches equivalent", "[point]") {
     chain::point expected;
-    BOOST_REQUIRE(entity_from_data(expected, valid_raw_point));
+    REQUIRE(entity_from_data(expected, valid_raw_point));
     chain::point instance;
 
     // This must be non-const.
     chain::point value;
 
-    BOOST_REQUIRE(entity_from_data(value, valid_raw_point));
+    REQUIRE(entity_from_data(value, valid_raw_point));
     instance = std::move(value);
-    BOOST_REQUIRE(instance == expected);
+    REQUIRE(instance == expected);
 }
 
-BOOST_AUTO_TEST_CASE(point__operator_assign_equals_2__always__matches_equivalent) {
+TEST_CASE("point  operator assign equals 2  always  matches equivalent", "[point]") {
     chain::point expected;
-    BOOST_REQUIRE(entity_from_data(expected, valid_raw_point));
+    REQUIRE(entity_from_data(expected, valid_raw_point));
     chain::point instance;
     instance = expected;
-    BOOST_REQUIRE(instance == expected);
+    REQUIRE(instance == expected);
 }
 
-BOOST_AUTO_TEST_CASE(point__operator_boolean_equals__duplicates__returns_true) {
+TEST_CASE("point  operator boolean equals  duplicates  returns true", "[point]") {
     chain::point alpha;
     chain::point beta;
-    BOOST_REQUIRE(entity_from_data(alpha, valid_raw_point));
-    BOOST_REQUIRE(entity_from_data(beta, valid_raw_point));
-    BOOST_REQUIRE(alpha == beta);
+    REQUIRE(entity_from_data(alpha, valid_raw_point));
+    REQUIRE(entity_from_data(beta, valid_raw_point));
+    REQUIRE(alpha == beta);
 }
 
-BOOST_AUTO_TEST_CASE(point__operator_boolean_equals__differs__returns_false) {
+TEST_CASE("point  operator boolean equals  differs  returns false", "[point]") {
     chain::point alpha;
     chain::point beta;
-    BOOST_REQUIRE(entity_from_data(alpha, valid_raw_point));
-    BOOST_REQUIRE_EQUAL(false, alpha == beta);
+    REQUIRE(entity_from_data(alpha, valid_raw_point));
+    REQUIRE(alpha != beta);
 }
 
-BOOST_AUTO_TEST_CASE(point__operator_boolean_not_equals__duplicates__returns_false) {
+TEST_CASE("point  operator boolean not equals  duplicates  returns false", "[point]") {
     chain::point alpha;
     chain::point beta;
-    BOOST_REQUIRE(entity_from_data(alpha, valid_raw_point));
-    BOOST_REQUIRE(entity_from_data(beta, valid_raw_point));
-    BOOST_REQUIRE_EQUAL(false, alpha != beta);
+    REQUIRE(entity_from_data(alpha, valid_raw_point));
+    REQUIRE(entity_from_data(beta, valid_raw_point));
+    REQUIRE(alpha == beta);
 }
 
-BOOST_AUTO_TEST_CASE(point__operator_boolean_not_equals__differs__returns_true) {
+TEST_CASE("point  operator boolean not equals  differs  returns true", "[point]") {
     chain::point alpha;
     chain::point beta;
-    BOOST_REQUIRE(entity_from_data(alpha, valid_raw_point));
-    BOOST_REQUIRE(alpha != beta);
+    REQUIRE(entity_from_data(alpha, valid_raw_point));
+    REQUIRE(alpha != beta);
 }
 
-BOOST_AUTO_TEST_CASE(point__checksum__all_ones__returns_all_ones) {
+TEST_CASE("point  checksum  all ones  returns all ones", "[point]") {
     static auto const tx_hash = hash_literal("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
     chain::point instance(tx_hash, 0xffffffff);
-    BOOST_REQUIRE_EQUAL(instance.checksum(), 0xffffffffffffffff);
+    REQUIRE(instance.checksum() == 0xffffffffffffffff);
 }
 
-BOOST_AUTO_TEST_CASE(point__checksum__all_zeroes__returns_all_zeroes) {
+TEST_CASE("point  checksum  all zeroes  returns all zeroes", "[point]") {
     static auto const tx_hash = hash_literal("0000000000000000000000000000000000000000000000000000000000000000");
     chain::point instance(tx_hash, 0x00000000);
-    BOOST_REQUIRE_EQUAL(instance.checksum(), 0x0000000000000000);
+    REQUIRE(instance.checksum() == 0x0000000000000000);
 }
 
-BOOST_AUTO_TEST_CASE(point__checksum__pattern_one__returns_expected) {
+TEST_CASE("point  checksum  pattern one  returns expected", "[point]") {
     static auto const tx_hash = hash_literal("000000000000000000000000aaaaaaaaaaaaaaaa000000000000000000000000");
     chain::point instance(tx_hash, 0x00000001);
-    BOOST_REQUIRE_EQUAL(instance.checksum(), 0xaaaaaaaaaaaa8001);
+    REQUIRE(instance.checksum() == 0xaaaaaaaaaaaa8001);
 }
 
-BOOST_AUTO_TEST_CASE(point__checksum__pattern_high__returns_expected) {
+TEST_CASE("point  checksum  pattern high  returns expected", "[point]") {
     static auto const tx_hash = hash_literal("ffffffffffffffffffffffff01234567aaaaaaaaffffffffffffffffffffffff");
     chain::point instance(tx_hash, 0x89abcdef);
-    BOOST_REQUIRE_EQUAL(instance.checksum(), 0x1234567aaaacdef);
+    REQUIRE(instance.checksum() == 0x1234567aaaacdef);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// End Boost Suite
