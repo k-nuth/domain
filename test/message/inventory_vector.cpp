105a4009cd0ac777c509f7800a5f5e606a14e7d9
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-24 19:24:00 -0300

    Never use Boost.Test again inventory_vector

--- test/message/inventory_vector.cpp
+++ test/message/inventory_vector.cpp
@@ -127,13 +127,13 @@ TEST_CASE("inventory vector  factory from data 2  valid input  success", "[inven
     auto const data = expected.to_data(version);
     data_source istream(data);
     auto const result = create<inventory_vector>(version, istream);
-    BOOST_REQUIRE(result.is_valid());
-    BOOST_REQUIRE(expected == result);
-    BOOST_REQUIRE_EQUAL(data.size(), result.serialized_size(version));
-    BOOST_REQUIRE_EQUAL(expected.serialized_size(version), result.serialized_size(version));
+    REQUIRE(result.is_valid());
+    REQUIRE(expected == result);
+    REQUIRE(data.size() == result.serialized_size(version));
+    REQUIRE(expected.serialized_size(version) == result.serialized_size(version));
 }
 
-BOOST_AUTO_TEST_CASE(inventory_vector__factory_from_data_3__valid_input__success) {
+TEST_CASE("inventory vector  factory from data 3  valid input  success", "[inventory vector]") {
     static inventory_vector const expected{
         inventory_vector::type_id::block,
         {{0x66, 0x9a, 0x0d, 0x24, 0x66, 0xd5, 0x39, 0x89,
