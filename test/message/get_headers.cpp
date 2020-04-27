48836e41b459c7778389ab61015b8284d7f84bb1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-27 20:54:00 -0300

    use Catch2 in get_headers

--- test/message/get_headers.cpp
+++ test/message/get_headers.cpp
@@ -53,13 +53,13 @@ TEST_CASE("get headers  constructor 4  always  equals params", "[get headers]") 
 
     const message::get_headers expected(starts, stop);
     message::get_headers instance(expected);
-    BOOST_REQUIRE(instance.is_valid());
-    BOOST_REQUIRE(expected == instance);
-    BOOST_REQUIRE(starts == instance.start_hashes());
-    BOOST_REQUIRE(stop == instance.stop_hash());
+    REQUIRE(instance.is_valid());
+    REQUIRE(expected == instance);
+    REQUIRE(starts == instance.start_hashes());
+    REQUIRE(stop == instance.stop_hash());
 }
 
-BOOST_AUTO_TEST_CASE(get_headers__constructor_5__always__equals_params) {
+TEST_CASE("get headers  constructor 5  always  equals params", "[get headers]") {
     hash_list starts = {
         hash_literal("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
         hash_literal("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"),
