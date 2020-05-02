d7759e66271eb0fd8cd9b7abaa36cbc199b867b6
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-02 19:28:00 -0300

    Replace Boost.Test with Catch2 in reject

--- test/message/reject.cpp
+++ test/message/reject.cpp
@@ -143,11 +143,11 @@ TEST_CASE("reject  from data  code obsolete  success", "[reject]") {
 
     data_chunk const raw = expected.to_data(version_maximum);
     message::reject instance{};
-    BOOST_REQUIRE(entity_from_data(instance, message::reject::version_minimum, raw));
-    BOOST_REQUIRE(expected == instance);
+    REQUIRE(entity_from_data(instance, message::reject::version_minimum, raw));
+    REQUIRE(expected == instance);
 }
 
-BOOST_AUTO_TEST_CASE(reject__from_data__code_duplicate__success) {
+TEST_CASE("reject  from data  code duplicate  success", "[reject]") {
     const message::reject expected(
         message::reject::reason_code::duplicate,
         message::block::command,
