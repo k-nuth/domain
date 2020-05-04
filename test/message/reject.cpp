0593fe80fb545d235b7ac1536aae50cc26ea657c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-04 20:51:00 -0300

    Never use Boost.Test again reject

--- test/message/reject.cpp
+++ test/message/reject.cpp
@@ -156,11 +156,11 @@ TEST_CASE("reject  from data  code duplicate  success", "[reject]") {
 
     data_chunk const raw = expected.to_data(version_maximum);
     message::reject instance{};
-    BOOST_REQUIRE(entity_from_data(instance, message::reject::version_minimum, raw));
-    BOOST_REQUIRE(expected == instance);
+    REQUIRE(entity_from_data(instance, message::reject::version_minimum, raw));
+    REQUIRE(expected == instance);
 }
 
-BOOST_AUTO_TEST_CASE(reject__from_data__code_nonstandard__success) {
+TEST_CASE("reject  from data  code nonstandard  success", "[reject]") {
     const message::reject expected(
         message::reject::reason_code::nonstandard,
         message::block::command,
