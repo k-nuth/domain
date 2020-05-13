5020e8547f8321fde7a6a74e9fccc4540ec91142
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-13 19:37:00 -0300

    Never use Boost.Test again block

--- test/message/block.cpp
+++ test/message/block.cpp
@@ -346,10 +346,10 @@ TEST_CASE("block  operator boolean equals 2  duplicates  returns true", "[messag
          chain::transaction(4, 16, {}, {})});
 
     message::block instance(expected);
-    BOOST_REQUIRE(instance == expected);
+    REQUIRE(instance == expected);
 }
 
-BOOST_AUTO_TEST_CASE(block__operator_boolean_equals_2__differs__returns_false) {
+TEST_CASE("block  operator boolean equals 2  differs  returns false", "[message block]") {
     const message::block expected(
         chain::header(10u,
                       hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
