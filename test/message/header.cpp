ddc87f26b6c8a88ad8505b8201c70e6c52dfd31c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-19 20:40:00 -0300

    use Catch2 in header

--- test/message/header.cpp
+++ test/message/header.cpp
@@ -322,10 +322,10 @@ TEST_CASE("message header  operator boolean not equals 2  duplicates  returns fa
         68644u);
 
     message::header instance(expected);
-    BOOST_REQUIRE_EQUAL(false, instance != expected);
+    REQUIRE(instance == expected);
 }
 
-BOOST_AUTO_TEST_CASE(header__operator_boolean_not_equals_2__differs__returns_true) {
+TEST_CASE("message header  operator boolean not equals 2  differs  returns true", "[message header]") {
     const message::header expected(
         10u,
         hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
