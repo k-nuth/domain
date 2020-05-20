783647362ac987158f6b8ef1e5f3dc6864889fd1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-19 22:04:00 -0300

    use Catch2 framework in header

--- test/chain/header.cpp
+++ test/chain/header.cpp
@@ -421,10 +421,10 @@ TEST_CASE("chain header  operator boolean equals  differs  returns false", "[cha
         68644u);
 
     chain::header instance;
-    BOOST_REQUIRE_EQUAL(false, instance == expected);
+    REQUIRE(instance != expected);
 }
 
-BOOST_AUTO_TEST_CASE(header__operator_boolean_not_equals__duplicates__returns_false) {
+TEST_CASE("chain header  operator boolean not equals  duplicates  returns false", "[chain header]") {
     chain::header const expected(
         10u,
         hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
