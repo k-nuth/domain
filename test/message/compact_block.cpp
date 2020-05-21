24a00f944b696f45596996bd0c99528334126bbe
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-21 19:42:00 -0300

    Never use Boost.Test again compact_block

--- test/message/compact_block.cpp
+++ test/message/compact_block.cpp
@@ -550,10 +550,10 @@ TEST_CASE("compact block  operator boolean equals  duplicates  returns true", "[
          message::prefilled_transaction(30, chain::transaction(4, 16, {}, {}))});
 
     message::compact_block instance(expected);
-    BOOST_REQUIRE(instance == expected);
+    REQUIRE(instance == expected);
 }
 
-BOOST_AUTO_TEST_CASE(compact_block__operator_boolean_equals__differs__returns_false) {
+TEST_CASE("compact block  operator boolean equals  differs  returns false", "[compact block]") {
     const message::compact_block expected(
         chain::header(10u,
                       hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
