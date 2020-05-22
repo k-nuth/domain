0ebc483c77f72197e803e9e883ed1cefea9aa43a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-22 19:26:00 -0300

    code improvements for input_point

--- include/kth/domain/chain/input_point.hpp
+++ include/kth/domain/chain/input_point.hpp
@@ -2,15 +2,15 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KTH_CHAIN_INPUT_POINT_HPP
-#define KTH_CHAIN_INPUT_POINT_HPP
+#ifndef KTH_DOMAIN_CHAIN_INPUT_POINT_HPP
+#define KTH_DOMAIN_CHAIN_INPUT_POINT_HPP
 
 #include <kth/domain/chain/point.hpp>
 
-namespace kth::chain {
+namespace kth::domain::chain {
 
 using input_point = point;
 
-}  // namespace kth::chain
+} // namespace kth::domain::chain
 
 #endif
