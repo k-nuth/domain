f754d9886277d7865e3515a0d72fc56c9bf1b0ed
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-22 21:42:00 -0300

    class heading needed to be normalized

--- include/kth/domain/message/headers.hpp
+++ include/kth/domain/message/headers.hpp
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KTH_MESSAGE_HEADERS_HPP
-#define KTH_MESSAGE_HEADERS_HPP
+#ifndef KTH_DOMAIN_MESSAGE_HEADERS_HPP
+#define KTH_DOMAIN_MESSAGE_HEADERS_HPP
 
 #include <cstdint>
 #include <initializer_list>
