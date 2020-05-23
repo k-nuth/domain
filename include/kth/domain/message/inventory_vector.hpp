cf6d736c6a18b606eaf97d2550622725747fffa3
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-22 21:48:00 -0300

    improvements in class inventory_vector

--- include/kth/domain/message/inventory_vector.hpp
+++ include/kth/domain/message/inventory_vector.hpp
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef KTH_MESSAGE_INVENTORY_VECTOR_HPP
-#define KTH_MESSAGE_INVENTORY_VECTOR_HPP
+#ifndef KTH_DOMAIN_MESSAGE_INVENTORY_VECTOR_HPP
+#define KTH_DOMAIN_MESSAGE_INVENTORY_VECTOR_HPP
 
 #include <cstdint>
 #include <istream>
