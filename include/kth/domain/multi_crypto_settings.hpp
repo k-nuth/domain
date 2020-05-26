bbdcf92f0753e22636bc4b614d70ed3bdccffe05
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 15:41:24 -0300

    Compile time Coin traits

---
+++ include/kth/domain/multi_crypto_settings.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2016-2020 Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef KTH_MULTI_CRYPTO_SETTINGS_HPP_
+#define KTH_MULTI_CRYPTO_SETTINGS_HPP_
+
+#ifndef KTH_CURRENCY_BCH
+#define KTH_SEGWIT_ENABLED
+#endif
+
+#endif /*KTH_MULTI_CRYPTO_SETTINGS_HPP_*/
