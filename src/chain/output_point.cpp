dab0714ac6d0f80294dffa7d0e13a0d9fee08516
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 19:56:00 -0300

    code improvements for output_point

--- src/chain/output_point.cpp
+++ src/chain/output_point.cpp
@@ -61,21 +61,6 @@ bool operator!=(output_point const& x, output_point const& y) {
     return !(x == y);
 }
 
-// Deserialization.
-//-----------------------------------------------------------------------------
-
-output_point output_point::factory_from_data(data_chunk const& data, bool wire) {
-    output_point instance;
-    instance.from_data(data, wire);
-    return instance;
-}
-
-output_point output_point::factory_from_data(std::istream& stream, bool wire) {
-    output_point instance;
-    instance.from_data(stream, wire);
-    return instance;
-}
-
 // Validation.
 //-----------------------------------------------------------------------------
 
