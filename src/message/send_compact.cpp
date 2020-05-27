019df58afc3f37f4a9f5eab6b45fc44bd711ae61
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:40:00 -0300

    improvements in class send_compact

--- src/message/send_compact.cpp
+++ src/message/send_compact.cpp
@@ -59,16 +59,6 @@ void send_compact::reset() {
     version_ = 0;
 }
 
-bool send_compact::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool send_compact::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk send_compact::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
