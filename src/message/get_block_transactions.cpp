c66d3fdc5aad09a705f94d0cb6ee25a05d6003d1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:09:00 -0300

    code style improvements for get_block_transactions

--- src/message/get_block_transactions.cpp
+++ src/message/get_block_transactions.cpp
@@ -72,16 +72,6 @@ void get_block_transactions::reset() {
     indexes_.shrink_to_fit();
 }
 
-bool get_block_transactions::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool get_block_transactions::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk get_block_transactions::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
