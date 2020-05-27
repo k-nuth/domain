3968a90aeb42c091a244dfe36a51f52ed86c75df
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:04:00 -0300

    class filter_clear needed to be C++17-ized

--- src/message/filter_clear.cpp
+++ src/message/filter_clear.cpp
@@ -30,16 +30,6 @@ void filter_clear::reset() {
     insufficient_version_ = true;
 }
 
-bool filter_clear::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool filter_clear::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk filter_clear::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
