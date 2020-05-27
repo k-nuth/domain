5e561d55f91d66a45dc4e6ade5ce50e906ab3773
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:26:00 -0300

    class memory_pool needed to be C++17-ized

--- src/message/memory_pool.cpp
+++ src/message/memory_pool.cpp
@@ -38,16 +38,6 @@ void memory_pool::reset() {
     insufficient_version_ = true;
 }
 
-bool memory_pool::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool memory_pool::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk memory_pool::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
