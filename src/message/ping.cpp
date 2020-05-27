bb99c52a73b5f39856d80c65fa9ae7f7446f2891
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:33:00 -0300

    class ping needed to be normalized

--- src/message/ping.cpp
+++ src/message/ping.cpp
@@ -44,16 +44,6 @@ bool ping::operator!=(ping const& x) const {
     return !(*this == x);
 }
 
-bool ping::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool ping::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk ping::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
