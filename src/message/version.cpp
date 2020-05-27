87710000f0ba31672f67ef295567f5172961d38c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:50:00 -0300

    some improvements to version class

--- src/message/version.cpp
+++ src/message/version.cpp
@@ -85,16 +85,6 @@ void version::reset() {
     relay_ = false;
 }
 
-bool version::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool version::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk version::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
