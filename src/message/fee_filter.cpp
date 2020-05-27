fb7c748067acf652346e75bbde9409d134d01cd1
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:00:00 -0300

    improvements in class fee_filter

--- src/message/fee_filter.cpp
+++ src/message/fee_filter.cpp
@@ -55,16 +55,6 @@ bool fee_filter::operator!=(fee_filter const& x) const {
     return !(*this == x);
 }
 
-bool fee_filter::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool fee_filter::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk fee_filter::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
