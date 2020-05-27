67fa3d375af249d79a86744e536f1a2e15abfc36
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-27 20:43:00 -0300

    class address needed to be normalized

--- include/kth/domain/message/address.hpp
+++ include/kth/domain/message/address.hpp
@@ -43,16 +43,16 @@ public:
     bool operator!=(address const& x) const;
 
 
-    network_address::list& addresses();
+    infrastructure::message::network_address::list& addresses();
 
-    [[nodiscard]] 
-    network_address::list const& addresses() const;
+    [[nodiscard]]
+    infrastructure::message::network_address::list const& addresses() const;
 
-    void set_addresses(network_address::list const& value);
-    void set_addresses(network_address::list&& value);
+    void set_addresses(infrastructure::message::network_address::list const& value);
+    void set_addresses(infrastructure::message::network_address::list&& value);
 
-    bool from_data(uint32_t version, data_chunk const& data);
-    bool from_data(uint32_t version, std::istream& stream);
+    // bool from_data(uint32_t version, data_chunk const& data);
+    // bool from_data(uint32_t version, std::istream& stream);
 
     template <typename R, KTH_IS_READER(R)>
     bool from_data(uint32_t version, R& source) {
