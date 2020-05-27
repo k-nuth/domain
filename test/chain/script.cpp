309dc79bac3bfc06660ccd8d261b2f762c4d2a15
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 23:09:00 -0300

    some improvements to script class

--- test/chain/script.cpp
+++ test/chain/script.cpp
@@ -124,21 +124,21 @@ BOOST_AUTO_TEST_CASE(script__from_data__testnet_119058_invalid_op_codes__success
     auto const raw_script = to_chunk(base16_literal("0130323066643366303435313438356531306633383837363437356630643265396130393739343332353534313766653139316438623963623230653430643863333030326431373463336539306366323433393231383761313037623634373337633937333135633932393264653431373731636565613062323563633534353732653302ae"));
 
     script parsed;
-    BOOST_REQUIRE(parsed.from_data(raw_script, false));
+    BOOST_REQUIRE(entity_from_data(parsed, raw_script, false));
 }
 
 BOOST_AUTO_TEST_CASE(script__from_data__parse__success) {
     auto const raw_script = to_chunk(base16_literal("3045022100ff1fc58dbd608e5e05846a8e6b45a46ad49878aef6879ad1a7cf4c5a7f853683022074a6a10f6053ab3cddc5620d169c7374cd42c1416c51b9744db2c8d9febfb84d01"));
 
     script parsed;
-    BOOST_REQUIRE(parsed.from_data(raw_script, true));
+    BOOST_REQUIRE(entity_from_data(parsed, raw_script, true));
 }
 
 BOOST_AUTO_TEST_CASE(script__from_data__to_data__roundtrips) {
     auto const normal_output_script = to_chunk(base16_literal("76a91406ccef231c2db72526df9338894ccf9355e8f12188ac"));
 
     script out_script;
-    BOOST_REQUIRE(out_script.from_data(normal_output_script, false));
+    BOOST_REQUIRE(entity_from_data(out_script, normal_output_script, false));
 
     auto const roundtrip = out_script.to_data(false);
     BOOST_REQUIRE(roundtrip == normal_output_script);
