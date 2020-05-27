ac2d08de2402960e959baa352b2b7464beaac48f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:43:00 -0300

    normalizes code in send_headers

--- src/message/send_headers.cpp
+++ src/message/send_headers.cpp
@@ -42,16 +42,6 @@ void send_headers::reset() {
     insufficient_version_ = true;
 }
 
-bool send_headers::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool send_headers::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk send_headers::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
