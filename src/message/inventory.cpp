6580545511bac3183c73f9ab83fdb5a546213e1b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-26 21:20:00 -0300

    normalizes code in inventory

--- src/message/inventory.cpp
+++ src/message/inventory.cpp
@@ -76,16 +76,6 @@ void inventory::reset() {
     inventories_.shrink_to_fit();
 }
 
-bool inventory::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool inventory::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk inventory::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
