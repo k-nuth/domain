44a4394d43f1ef1450eaa8e5a76e288835951319
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-27 20:37:00 -0300

    normalizes code in opcode

--- include/kth/domain/machine/opcode.hpp
+++ include/kth/domain/machine/opcode.hpp
@@ -306,19 +306,17 @@ enum class opcode : uint8_t {
 };
 
 /// Convert the opcode to a mnemonic string.
-BI_API std::string opcode_to_string(opcode value, uint32_t active_forks);
+KD_API std::string opcode_to_string(opcode value, uint32_t active_forks);
 
 /// Convert a string to an opcode.
-BI_API bool opcode_from_string(opcode& out_code, std::string const& value);
+KD_API bool opcode_from_string(opcode& out_code, std::string const& value);
 
 /// Convert any opcode to a string hexadecimal representation.
-BI_API std::string opcode_to_hexadecimal(opcode code);
+KD_API std::string opcode_to_hexadecimal(opcode code);
 
 /// Convert any hexadecimal byte to an opcode.
-BI_API bool opcode_from_hexadecimal(opcode& out_code,
-    std::string const& value);
+KD_API bool opcode_from_hexadecimal(opcode& out_code, std::string const& value);
 
-} // namespace machine
-} // namespace kth
+} // namespace kth::domain::machine
 
-#endif // KTH_MACHINE_OPCODE_HPP_
+#endif // KTH_DOMAIN_MACHINE_OPCODE_HPP
