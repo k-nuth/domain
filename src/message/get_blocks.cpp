14b83e2654af205bb29abe4f6d52489a7d2a1db5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-27 23:22:00 -0300

    code modernization for get_blocks

--- src/message/get_blocks.cpp
+++ src/message/get_blocks.cpp
@@ -55,16 +55,6 @@ void get_blocks::reset() {
     stop_hash_.fill(0);
 }
 
-bool get_blocks::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool get_blocks::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk get_blocks::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
