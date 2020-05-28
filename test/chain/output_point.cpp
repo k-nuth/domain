eaf21faa8a52f6c24250041d5e288103b27bd9c5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-28 19:20:00 -0300

    C++17-izes code in output_point

--- test/chain/output_point.cpp
+++ test/chain/output_point.cpp
@@ -96,7 +96,7 @@ BOOST_AUTO_TEST_CASE(output_point__from_data__roundtrip__success) {
     chain::output_point point;
 
     BOOST_REQUIRE(point != initial);
-    BOOST_REQUIRE(point.from_data(initial.to_data()));
+    BOOST_REQUIRE(entity_from_data(point, initial.to_data()));
     BOOST_REQUIRE(point.is_valid());
     BOOST_REQUIRE(point == initial);
 }
