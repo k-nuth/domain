d169b382aa765c5ad407d8a1ddb4bb3fa5619e5f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-27 23:47:00 -0300

    C++17-izes code in prefilled_transaction

--- src/message/prefilled_transaction.cpp
+++ src/message/prefilled_transaction.cpp
@@ -68,16 +68,6 @@ void prefilled_transaction::reset() {
     transaction_ = chain::transaction{};
 }
 
-bool prefilled_transaction::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool prefilled_transaction::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk prefilled_transaction::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
