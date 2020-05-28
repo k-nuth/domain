41c67bf2a7a163436797f48f59687f7673daf87f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-27 21:31:00 -0300

    code C++17-ization for not_found

--- include/kth/domain/message/not_found.hpp
+++ include/kth/domain/message/not_found.hpp
@@ -45,12 +45,11 @@ public:
     bool operator!=(not_found const& x) const;
 
 
-    bool from_data(uint32_t version, data_chunk const& data); /*override*/  //TODO(fernando): check if this function is used in a run-time-polymorphic way
-    bool from_data(uint32_t version, std::istream& stream); /*override*/     //TODO(fernando): check if this function is used in a run-time-polymorphic way
+    // bool from_data(uint32_t version, data_chunk const& data); /*override*/  //TODO(fernando): check if this function is used in a run-time-polymorphic way
+    // bool from_data(uint32_t version, std::istream& stream); /*override*/     //TODO(fernando): check if this function is used in a run-time-polymorphic way
 
     template <typename R, KTH_IS_READER(R)>
-    bool from_data(uint32_t version, R& source) /*override*/  //TODO(fernando): check if this function is used in a run-time-polymorphic way
-    {
+    bool from_data(uint32_t version, R& source) {
         if ( ! inventory::from_data(version, source)) {
             return false;
         }
