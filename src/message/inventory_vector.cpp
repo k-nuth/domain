5cd16e79aca90f9fac1b839458e9b30b063455fc
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-28 23:48:00 -0300

    code normalization for inventory_vector

--- src/message/inventory_vector.cpp
+++ src/message/inventory_vector.cpp
@@ -78,16 +78,6 @@ void inventory_vector::to_witness() {
 }
 #endif
 
-bool inventory_vector::from_data(uint32_t version, data_chunk const& data) {
-    data_source istream(data);
-    return from_data(version, istream);
-}
-
-bool inventory_vector::from_data(uint32_t version, std::istream& stream) {
-    istream_reader stream_r(stream);
-    return from_data(version, stream_r);
-}
-
 data_chunk inventory_vector::to_data(uint32_t version) const {
     data_chunk data;
     auto const size = serialized_size(version);
