8ea290d9c10d252a454c95e3c3720a0bfcd77cfd
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-29 19:35:00 -0300

    class address needed to be modernized

--- test/message/address.cpp
+++ test/message/address.cpp
@@ -80,7 +80,7 @@ BOOST_AUTO_TEST_CASE(address__constructor_3__always__equals_params) {
 }
 
 BOOST_AUTO_TEST_CASE(address__constructor_4__always__equals_params) {
-    network_address::list const addresses{
+    infrastructure::message::network_address::list const addresses{
         network_address(
             734678u,
             5357534u,
