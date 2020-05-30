0d21f3636485146f511ab8d05821b432088a412d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-30 19:06:00 -0300

    class inventory needed to be normalized

--- test/message/inventory.cpp
+++ test/message/inventory.cpp
@@ -152,7 +152,7 @@ BOOST_AUTO_TEST_CASE(inventory__factory_from_data_3__valid_input__success) {
     auto const data = expected.to_data(version);
     data_source istream(data);
     istream_reader source(istream);
-    auto const result = inventory::factory_from_data(version, source);
+    auto const result = create<inventory>(version, source);
     BOOST_REQUIRE(result.is_valid());
     BOOST_REQUIRE(expected == result);
     BOOST_REQUIRE_EQUAL(data.size(), result.serialized_size(version));
