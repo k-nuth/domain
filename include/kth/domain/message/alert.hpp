d04c8939d78c610666746ff1837beca21b567d81
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-29 21:30:00 -0300

    code C++17-ization for alert

--- include/kth/domain/message/alert.hpp
+++ include/kth/domain/message/alert.hpp
@@ -67,20 +67,20 @@ public:
 
         if ( ! source) {
             reset();
-}
+        }
 
         return source;
     }
 
     //bool from_data(uint32_t version, reader& source);
 
-    [[nodiscard]] 
+    [[nodiscard]]
     data_chunk to_data(uint32_t version) const;
     
     void to_data(uint32_t version, data_sink& stream) const;
 
     template <typename W>
-    void to_data(uint32_t  /*version*/, W& sink) const {
+    void to_data(uint32_t /*version*/, W& sink) const {
         sink.write_variable_little_endian(payload_.size());
         sink.write_bytes(payload_);
         sink.write_variable_little_endian(signature_.size());
