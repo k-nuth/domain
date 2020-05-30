05f4e2ecc595f998f4003419ed789cd80bbfc82d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-29 21:37:00 -0300

    class compact_block needed to be C++17-ized

--- include/kth/domain/message/compact_block.hpp
+++ include/kth/domain/message/compact_block.hpp
@@ -65,14 +65,14 @@ public:
 
     prefilled_transaction::list& transactions();
     
-    [[nodiscard]] 
+    [[nodiscard]]
     prefilled_transaction::list const& transactions() const;
     
     void set_transactions(prefilled_transaction::list const& value);
     void set_transactions(prefilled_transaction::list&& value);
 
-    bool from_data(uint32_t version, data_chunk const& data);
-    bool from_data(uint32_t version, std::istream& stream);
+    // bool from_data(uint32_t version, data_chunk const& data);
+    // bool from_data(uint32_t version, std::istream& stream);
 
     template <typename R, KTH_IS_READER(R)>
     bool from_data(uint32_t version, R& source) {
