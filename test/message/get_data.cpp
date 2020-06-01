b0666ffdb88f34e352428618804ab694345ca9b7
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-31 23:47:00 -0300

    improvements in class get_data

--- test/message/get_data.cpp
+++ test/message/get_data.cpp
@@ -153,7 +153,7 @@ BOOST_AUTO_TEST_CASE(get_data__factory_from_data_3__valid_input__success) {
     auto const data = expected.to_data(version);
     data_source istream(data);
     istream_reader source(istream);
-    auto const result = get_data::factory_from_data(version, source);
+    auto const result = create<get_data>(version, source);
     BOOST_REQUIRE(result.is_valid());
     BOOST_REQUIRE(expected == result);
     BOOST_REQUIRE_EQUAL(data.size(), result.serialized_size(version));
