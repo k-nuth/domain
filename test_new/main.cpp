ff647e5d0ad5497b2be50b11f9e2f6f85e782858
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-01 23:03:00 -0300

    class main needed to be normalized

--- test_new/main.cpp
+++ test_new/main.cpp
@@ -103,8 +103,8 @@ TEST_CASE("[test_read_null_terminated_string] 5") {
 
 TEST_CASE("[test_read_null_terminated_string] 6") {
     data_chunk data = {'A', 'B', 'C', 0};
-    bc::data_source ds(data);
-    bc::istream_reader source(ds);
+    kth::data_source ds(data);
+    kth::istream_reader source(ds);
 
     auto ret = read_null_terminated_string(source, 0);
     REQUIRE(!bool(ret));
