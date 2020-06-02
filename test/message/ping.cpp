b018098b097d0343f5945d6eeeabbb91950f8920
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-01 21:58:00 -0300

    C++17-izes code in ping

--- test/message/ping.cpp
+++ test/message/ping.cpp
@@ -124,7 +124,7 @@ BOOST_AUTO_TEST_CASE(ping__factory_from_data_1__bip31_version_round_trip__expect
 
     static auto const version = message::version::level::bip31;
     auto const data = expected.to_data(version);
-    auto const result = message::ping::factory_from_data(version, data);
+    auto const result = create<message::ping>(version, data);
     BOOST_REQUIRE(result.is_valid());
     BOOST_REQUIRE(result == expected);
 }
