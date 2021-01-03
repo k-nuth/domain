cc664ea1d775dfc7c3b59bc8c3db3d24829325a4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-01-03 19:33:00 -0300

    send_headers needed to be C++17-ized

--- src/message/send_headers.cpp
+++ src/message/send_headers.cpp
@@ -25,14 +25,6 @@ send_headers::send_headers(bool insufficient_version)
     : insufficient_version_(insufficient_version) {
 }
 
-// send_headers::send_headers(send_headers const& x)
-//     : send_headers(x.insufficient_version_) {
-// }
-
-// send_headers::send_headers(send_headers&& x) noexcept
-//     : send_headers(x.insufficient_version_) 
-// {}
-
 bool send_headers::is_valid() const {
     return !insufficient_version_;
 }
