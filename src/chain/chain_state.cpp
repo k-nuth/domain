fd68d596d752bd0a448eb0159b651e690e488c66
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-01-07 22:36:00 -0300

    chain_state needed to be modernized

--- src/chain/chain_state.cpp
+++ src/chain/chain_state.cpp
@@ -723,10 +723,8 @@ bool chain_state::is_euler_enabled(size_t height, config::network network) {
 //     return is_rule_enabled(height, network
 //         , mainnet_gauss_activation_height
 //         , testnet_gauss_activation_height
-// #if defined(KTH_CURRENCY_BCH)
 //         , testnet4_gauss_activation_height
 //         , scalenet_gauss_activation_height
-// #endif
 //      );
 // }
 
