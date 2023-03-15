44bbfa1dddf03f8001eaa617c590cbf0ee1d4397
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-03-15 17:25:42 +0100

    fix

--- conanfile.py
+++ conanfile.py
@@ -64,8 +64,10 @@ class KnuthDomainConan(KnuthConanFileV2):
             self.test_requires("catch2/3.3.1")
 
     def requirements(self):
-        self.requires("fmt/9.1.0")
         self.requires("infrastructure/0.24.0")
+
+        self.requires("boost/1.81.0")
+        self.requires("fmt/9.1.0")
 
         if self.options.currency == "LTC":
             self.requires("OpenSSL/1.0.2l@conan/stable")
