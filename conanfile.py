955f90602a350df6d427c2e09501d4ace05c92b7
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-11-13 18:09:58 +0100

    upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -64,7 +64,7 @@ class KnuthDomainConan(KnuthConanFileV2):
             self.test_requires("catch2/3.3.2")
 
     def requirements(self):
-        self.requires("infrastructure/0.30.0", transitive_headers=True, transitive_libs=True)
+        self.requires("infrastructure/0.31.0", transitive_headers=True, transitive_libs=True)
 
         if self.options.currency == "LTC":
             self.requires("OpenSSL/1.0.2l@conan/stable", transitive_headers=True, transitive_libs=True)
