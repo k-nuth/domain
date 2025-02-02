098fb8aeef7b145c373f43d4d41079a4564facd5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-02-02 21:12:57 +0100

    release v0.42.0

--- conanfile.py
+++ conanfile.py
@@ -62,7 +62,7 @@ class KnuthDomainConan(KnuthConanFileV2):
             self.test_requires("catch2/3.6.0")
 
     def requirements(self):
-        self.requires("infrastructure/0.39.0", transitive_headers=True, transitive_libs=True)
+        self.requires("infrastructure/0.40.0", transitive_headers=True, transitive_libs=True)
 
         if self.options.currency == "LTC":
             self.requires("OpenSSL/1.0.2l@conan/stable", transitive_headers=True, transitive_libs=True)
