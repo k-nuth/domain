133072f7b7b36f95e7c7dfadc8d685a38dc95bcf
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-04-15 16:45:02 +0200

    release v0.43.0

--- conanfile.py
+++ conanfile.py
@@ -62,7 +62,7 @@ class KnuthDomainConan(KnuthConanFileV2):
             self.test_requires("catch2/3.6.0")
 
     def requirements(self):
-        self.requires("infrastructure/0.40.0", transitive_headers=True, transitive_libs=True)
+        self.requires("infrastructure/0.41.0", transitive_headers=True, transitive_libs=True)
         self.requires("tiny-aes-c/1.0.0", transitive_headers=True, transitive_libs=True)
 
         if self.options.currency == "LTC":
